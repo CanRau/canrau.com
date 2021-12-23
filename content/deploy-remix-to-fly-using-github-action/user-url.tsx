65b53d7fa220e1c4b501b0504567abe5fc89178e
CanRau <cansrau@gmail.com>
2021-12-23 18:05:48 -0500

    chore: fix types and style
    Intellisense wouldn't work yesterday 😢
    
    from: https://github.com/angular/angular/blob/22b96b9/CONTRIBUTING.md#type
    from: https://www.conventionalcommits.org/en/v1.0.0/
    from: https://cheatography.com/albelop/cheat-sheets/conventional-commits/
    
    from: https://gist.github.com/lisawolderiksen/a7b99d94c92c6671181611be1641c733

--- content/deploy-remix-to-fly-using-github-action/user-url.tsx
+++ content/deploy-remix-to-fly-using-github-action/user-url.tsx
@@ -1,39 +1,51 @@
-import {useState, useRef} from "react"
+import {
+  useState,
+  useRef,
+  type ChangeEvent,
+  type ReactNode,
+  type KeyboardEvent,
+} from "react";
 
 type Props = {
   initial: string;
   prefix: string;
   suffix: string;
-}
+};
 
-const Wrapper = ({children}) => <div>{children}</div>
+// ReactNode via https://www.carlrippon.com/react-children-with-typescript/
+const Wrapper = ({ children }: { children: ReactNode }) => (
+  <div>{children}</div>
+);
 
-export const UserUrl = ({initial = "", prefix = "", suffix = ""}: Props) => {
+export const UserUrl = ({ initial = "", prefix = "", suffix = "" }: Props) => {
   const [value, setValue] = useState("");
   const link = useRef();
-  
-  const onChange = (e) => {
-    e.preventDefault()
-    setValue(e.target.value)
-  }
-  
-  const onKeyUp = (e) => {
-    e.preventDefault()
+
+  // types from https://stackoverflow.com/a/70095204/3484824
+  const onChange = (e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    e.preventDefault();
+    setValue(e.target.value);
+  };
+
+  const onKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
+    e.preventDefault();
     if (e.code === "Enter") {
-      console.log(link.current.click())
+      (link?.current as unknown as HTMLAnchorElement)?.click();
     }
-  }
-  
-  const input = (<div>
-    <input
-      className="w-full bg-zinc-300 text-black placeholder:text-zinc-500 dark:bg-zinc-700 dark:text-zinc-200 dark:placeholder:text-zinc-400 px-4 py-2 mt-1 rounded-md"
-      type="text"
-      placeholder={initial}
-      value={value}
-      onChange={onChange}
-      onKeyUp={onKeyUp}
-    />
-  </div>)
+  };
+
+  const input = (
+    <div>
+      <input
+        className="w-full bg-zinc-300 text-black placeholder:text-zinc-500 dark:bg-zinc-700 dark:text-zinc-200 dark:placeholder:text-zinc-400 px-4 py-2 mt-1 rounded-md"
+        type="text"
+        placeholder={initial}
+        value={value}
+        onChange={onChange}
+        onKeyUp={onKeyUp}
+      />
+    </div>
+  );
 
   // value needs to include at least 1 character followed by / and a nother character
   if (/[^/<>?]+\/[^/<>?]+/.test(value)) {
@@ -41,20 +53,27 @@ export const UserUrl = ({initial = "", prefix = "", suffix = ""}: Props) => {
       <Wrapper>
         {input}
         <div className="mt-2">
-          <a ref={link} href={`${prefix}${value || initial}${suffix}`} target="_blank" rel="noopener noreferrer">
+          <a
+            ref={link.current}
+            href={`${prefix}${value || initial}${suffix}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             hit enter or click to go to your repo's settings
           </a>
         </div>
       </Wrapper>
-    )
+    );
   }
 
   return (
     <Wrapper>
       {input}
       <div className="mt-2">
-        <span>type your GitHub username/repo to generate the settings link</span>
+        <span>
+          type your GitHub username/repo to generate the settings link
+        </span>
       </div>
     </Wrapper>
-  )
-}
+  );
+};
