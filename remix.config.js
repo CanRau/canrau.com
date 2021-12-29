f343c9f39ccfdf1486b9f5d480e35d4f6e4842b1
CanRau <cansrau@gmail.com>
2021-12-29 06:19:13 -0500

    feat: add link to KCD sitemap creation

--- remix.config.js
+++ remix.config.js
@@ -7,6 +7,7 @@ module.exports = {
   publicPath: "/build/",
   serverBuildDirectory: "build",
   devServerPort: 8002,
-  // serverPlatform​: ​'neutral'​, // found in https://discord.com/channels/770287896669978684/923127300789248051/923130439626002453
+  // serverPlatform: ​"neutral"​, // found in https://discord.com/channels/770287896669978684/923127300789248051/923130439626002453
+  // serverPlatform: ​"node"​,
   // serverModuleFormat: "esm", // todo: verify [serverModuleFormat](https://github.com/remix-run/remix/pull/976)
 };
