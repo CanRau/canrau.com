7310f5bc33338f3493c58ad01cc115cdbee101df
CanRau <cansrau@gmail.com>
2022-01-10 14:39:48 -0500

    feat: generate ogimage on pre-commit

--- app/routes/$lang/__other/ogimage/$slug[.]png.tsx
+++ app/routes/$lang/__other/ogimage/$slug[.]png.tsx
@@ -1,7 +1,7 @@
 import { mkdir, readFile, writeFile } from "fs/promises";
 import { json, type LoaderFunction } from "remix";
-import puppeteer from "puppeteer";
-import sharp from "sharp";
+// import puppeteer from "puppeteer";
+// import sharp from "sharp";
 import { defaultLang, isContainer } from "/config";
 import { Lang } from "/types";
 import { join } from "path";
@@ -17,90 +17,86 @@ const defaultViewport = {
 
 export const loader: LoaderFunction = async ({ request, params }) => {
   const { slug } = params;
+  if (!slug) return null;
   const lang = (params.lang || defaultLang) as Lang;
   const url = new URL(request.url);
   const querySize = url.searchParams.get("size");
   const size = querySize ? sizes[querySize] : "";
+  const sizeSuffix = size ? `_${querySize}` : "";
+  const postPath = join(process.cwd(), "content", slug);
+  const imagePath = `${postPath}/ogimage${sizeSuffix}.png`;
   const headers: HeadersInit = {
     "Content-Type": "image/png",
     // can be `inline` or `attachment`
-    "Content-Disposition": `inline; filename="${slug}_ogimage.png"`,
+    "Content-Disposition": `inline; filename="${slug}_ogimage${sizeSuffix}.png"`,
     "x-content-type-options": "nosniff",
   };
-  const sizeSuffix = size ? `-${querySize}` : "";
-  const ogCache = join(process.cwd(), ".cache", "ogimages");
-  const imagePath = `${ogCache}/${slug}_ogimage${sizeSuffix}.png`;
-  // note pptrCache to prevent [`ENOSPC: no space left on device`](https://github.com/puppeteer/puppeteer/issues/6414)
-  const pptrCache = join(process.cwd(), ".cache", "pptr");
-  // note: `catch`ing on `mkdir` because it errors if dir already exists
-  await Promise.all([
-    mkdir(pptrCache, { recursive: true }).catch(() => {}),
-    mkdir(ogCache, { recursive: true }).catch(() => {}),
-  ]);
   const cachedImage = await readFile(imagePath).catch(() => {});
-  if (cachedImage) {
-    return new Response(cachedImage, { headers });
+  if (!cachedImage) {
+    console.log(`MISSING OG:IMAGE for "/${lang}/${slug}"`);
+    throw json({ lang, error: "missing image" }, 500);
   }
-  const launchBrowser = puppeteer.launch({
-    // note(Puppeteer): [--disable-dev-shm-usage](https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#tips)
-    // note(Puppeteer): --no-sandbox as mentionded on [SO](https://stackoverflow.com/a/59154049/3484824)
-    args: [
-      "--font-render-hinting=none", // from https://docs.browserless.io/blog/2020/09/30/puppeteer-print.html#use-a-special-launch-flag
-      "--disable-dev-shm-usage",
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-    ],
-    userDataDir: pptrCache,
-    ...(isContainer && { executablePath: "google-chrome-stable" }),
-  });
-  const browser = await launchBrowser;
-  const page = await browser.newPage();
-  // from https://docs.browserless.io/blog/2020/09/30/puppeteer-print.html#set-a-standard-user-agent-header
-  await page.setUserAgent(
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
-  );
-  const templateUrl = request.url.replace(".png", "/template");
+  return new Response(cachedImage, { headers });
+  // const launchBrowser = puppeteer.launch({
+  //   // note(Puppeteer): [--disable-dev-shm-usage](https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#tips)
+  //   // note(Puppeteer): --no-sandbox as mentionded on [SO](https://stackoverflow.com/a/59154049/3484824)
+  //   args: [
+  //     "--font-render-hinting=none", // from https://docs.browserless.io/blog/2020/09/30/puppeteer-print.html#use-a-special-launch-flag
+  //     "--disable-dev-shm-usage",
+  //     "--no-sandbox",
+  //     "--disable-setuid-sandbox",
+  //   ],
+  //   userDataDir: pptrCache,
+  //   ...(isContainer && { executablePath: "google-chrome-stable" }),
+  // });
+  // const browser = await launchBrowser;
+  // const page = await browser.newPage();
+  // // from https://docs.browserless.io/blog/2020/09/30/puppeteer-print.html#set-a-standard-user-agent-header
+  // await page.setUserAgent(
+  //   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
+  // );
+  // const templateUrl = request.url.replace(".png", "/template");
 
-  // await page.evaluate(() => (document.body.style.background = "transparent"));
-  // setViewport via [Peter Tran](https://petertran.com.au/2018/07/12/blank-images-puppeteer-screenshots-solved/)
-  // await page.setViewport(defaultViewport); // not needed at the moment
-  await page.goto(templateUrl);
-  const element = await page.$("#ogimage");
-  if (!element) {
-    console.error("Could'nt get #ogimage");
-    throw json({ lang, error: "Error creating the image" }, 500);
-  }
-  const boundingBox = await element.boundingBox();
-  if (!boundingBox) {
-    console.error("Could'nt get element.boundingBox");
-    throw json({ lang, error: "Error creating the image" }, 500);
-  }
-  let screenshot = await page?.screenshot({
-    omitBackground: true,
-    type: "png",
-    // done: screenshot ended up being cut-off without clip 😳
-    clip: { ...boundingBox, height: boundingBox.height },
-  });
-  await element.dispose();
-  await browser.close();
+  // // await page.evaluate(() => (document.body.style.background = "transparent"));
+  // // setViewport via [Peter Tran](https://petertran.com.au/2018/07/12/blank-images-puppeteer-screenshots-solved/)
+  // // await page.setViewport(defaultViewport); // not needed at the moment
+  // await page.goto(templateUrl);
+  // const element = await page.$("#ogimage");
+  // if (!element) {
+  //   console.error("Could'nt get #ogimage");
+  //   throw json({ lang, error: "Error creating the image" }, 500);
+  // }
+  // const boundingBox = await element.boundingBox();
+  // if (!boundingBox) {
+  //   console.error("Could'nt get element.boundingBox");
+  //   throw json({ lang, error: "Error creating the image" }, 500);
+  // }
+  // let screenshot = await page?.screenshot({
+  //   omitBackground: true,
+  //   type: "png",
+  //   // done: screenshot ended up being cut-off without clip 😳
+  //   clip: { ...boundingBox, height: boundingBox.height },
+  // });
+  // await element.dispose();
+  // await browser.close();
 
-  if (size) {
-    // twitter rendered in browser w504px X h263.867px
-    screenshot = await sharp(screenshot)
-      .resize(size)
-      .toBuffer()
-      .catch((e) => {
-        console.error(e);
-        throw json({ lang, error: "Error creating the image" }, 500);
-      });
-  }
+  // if (size) {
+  //   // twitter rendered in browser w504px X h263.867px
+  //   screenshot = await sharp(screenshot)
+  //     .resize(size)
+  //     .toBuffer()
+  //     .catch((e) => {
+  //       console.error(e);
+  //       throw json({ lang, error: "Error creating the image" }, 500);
+  //     });
+  // }
 
-  if (typeof screenshot === "undefined") {
-    throw json({ lang, error: "Error creating the image" }, 500);
-  }
+  // if (typeof screenshot === "undefined") {
+  //   throw json({ lang, error: "Error creating the image" }, 500);
+  // }
 
-  // note: would be cool if we could `Response` without returning so we could cache after sending the response to the browser
-  await writeFile(imagePath, screenshot);
+  // // note: would be cool if we could `Response` without returning so we could cache after sending the response to the browser
+  // await writeFile(imagePath, screenshot);
 
-  return new Response(screenshot, { headers });
+  // return new Response(screenshot, { headers });
 };
---
+++ utils/generate-ogimages.js
@@ -0,0 +1,98 @@
+const { readdir, readFile, writeFile } = require("fs").promises;
+const puppeteer = require("puppeteer");
+// const sharp = require("sharp");
+const [_, __, cmd, ...postPaths] = process.argv;
+
+let cachedBrowser;
+
+if (cmd === "all") {
+  readdir("./content")
+    .then(takeScreenshotOfFiles)
+    .then(() => console.log("done"));
+} else if (cmd === "pre-commit") {
+  const contentPaths = postPaths.filter((p) => p.includes("content"));
+  if (contentPaths.length === 0) {
+    return process.exit();
+  }
+  const files = contentPaths.map((p) =>
+    p.replace("content/", "").replace("/en.mdx", ""),
+  );
+  takeScreenshotOfFiles(files).then(() => console.log("done"));
+} else if (cmd === "single") {
+  console.log(`taking screenshot of ${postPaths[0]}`);
+  takeScreenshot(postPaths[0]).then(async () => {
+    console.log(`took screenshot of ${postPaths[0]}`);
+    if (cachedBrowser) {
+      console.log("closing browser instance");
+      await cachedBrowser.close();
+    }
+  });
+}
+
+async function takeScreenshotOfFiles(files) {
+  console.log("taking all screenshots");
+  for (const file of files) {
+    console.log(`taking screenshot of ${file}`);
+    await takeScreenshot(file);
+    console.log(`took screenshot of ${file}`);
+  }
+  if (cachedBrowser) {
+    console.log("closing browser instance");
+    await cachedBrowser.close();
+  }
+}
+
+async function getBrowser() {
+  console.log("launching browser");
+  return puppeteer.launch({
+    // headless: false,
+    // slowMo: 750,
+    executablePath:
+      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+  });
+}
+
+async function takeScreenshot(slug) {
+  const browser = cachedBrowser ?? (await getBrowser());
+  cachedBrowser = browser;
+  const template = `http://localhost:3000/en/ogimage/${slug}/template`;
+  const page = await browser.newPage();
+  await page.setUserAgent(
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
+  );
+  await page.goto(template, { waitUntil: "networkidle0" });
+  // await page.goto(url, { waitUntil: 'load' });
+  // await page.goto(url, { waitUntil: 'domcontentloaded' });
+  // await page.goto(url, { waitUntil: 'networkidle0' });
+  // await page.goto(url, { waitUntil: 'networkidle2' });
+
+  const element = await page.$("#ogimage");
+  if (!element) {
+    console.error("Could'nt get #ogimage");
+    process.exit();
+  }
+
+  const boundingBox = await element.boundingBox();
+  if (!boundingBox) {
+    console.error("Could'nt get element.boundingBox");
+    process.exit();
+  }
+
+  const path = `./content/${slug}/ogimage.png`;
+  await element?.screenshot({
+    omitBackground: true,
+    type: "png",
+    path,
+  });
+
+  // await sharp(screenshot)
+  //     .resize(size)
+  //     .toBuffer()
+  //     .catch((e) => {
+  //       console.error(e);
+  //       throw json({ lang, error: "Error creating the image" }, 500);
+  //     });
+
+  await element.dispose();
+  await page.close();
+}
