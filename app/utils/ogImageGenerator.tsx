3c3109a600f66c4601a5b21be78701f1807c5273
CanRau <cansrau@gmail.com>
2022-05-13 13:44:37 -0500

    feat(seo): increment og:image version v3

--- app/utils/ogImageGenerator.tsx
+++ app/utils/ogImageGenerator.tsx
@@ -5,7 +5,7 @@ import sharp from "sharp";
 import { Lang } from "/types";
 import { readFile, join } from "../utils.server";
 
-export const OG_IMAGE_VERSION = 2;
+export const OG_IMAGE_VERSION = 3;
 
 export type Size = "default" | "small";
 
