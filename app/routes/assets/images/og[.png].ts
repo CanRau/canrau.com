9136301864ac69d10c94531e116b6ff00f9fea63
CanRau <cansrau@gmail.com>
2022-05-18 10:47:40 -0500

    feat: use search params for og:image resource routes

--- app/routes/assets/images/og[.png].ts
+++ app/routes/assets/images/og[.png].ts
@@ -4,7 +4,7 @@ import matter from "gray-matter";
 import { getContentPath, getFilePath } from "~/utils/compile-mdx.server";
 // import { notFoundError } from "~/utils/error-responses";
 import { readFile, revHash } from "~/utils.server";
-import { languages, defaultLang } from "/config";
+import { domain, languages, defaultLang } from "/config";
 import type { Lang } from "/types";
 import { type Frontmatter } from "~/utils/mdx.server";
 import {
@@ -15,23 +15,16 @@ import {
   type Size,
 } from "~/utils/ogImageGenerator";
 
-type LoaderFunc<Params extends Record<string, unknown> = Record<string, unknown>> = (
-  args: Omit<Parameters<LoaderFunction>["0"], "params"> & { params: Params },
-) => ReturnType<LoaderFunction>;
-
-type Ctx = {
-  slug: string;
-  size: Size;
-  version: string;
-  rev: string;
-  lang: Lang;
-};
-
 // inspiration distribution in canvas http://jsfiddle.net/mes2L9vf/1/
-export const loader: LoaderFunc<Ctx> = async ({ params, request }) => {
-  const { slug, size, version, rev, lang } = params;
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const slug = url.searchParams.get("slug") ?? "";
+  const size = (url.searchParams.get("size") ?? "") as Size;
+  const version = url.searchParams.get("v") ?? "";
+  const rev = url.searchParams.get("rev") ?? "";
+  const lang = (url.searchParams.get("lang") ?? "") as Lang;
 
-  // if `slug` is missing we don't know what is requested so the stop right here and throw a 404
+  // if `slug` is missing we don't know what is requested so we stop right here and throw a 404
   // note: disabled version mismatch for now, not sure that's what caused Twitter Card Validator to fail tho 🤔
   if (!slug) {
     throw new Response("Not Found", { status: 404 });
@@ -40,22 +33,19 @@ export const loader: LoaderFunc<Ctx> = async ({ params, request }) => {
 
   // if `version` doesn't match, redirect to current `OG_IMAGE_VERSION`
   if (parseInt(version, 10) !== OG_IMAGE_VERSION) {
-    const url = new URL(request.url);
-    url.pathname = `/assets/${lang}/ogimage/v${OG_IMAGE_VERSION}/${size}/${slug}.${rev}.png`;
+    url.searchParams.set("v", `${OG_IMAGE_VERSION}`);
     return redirect(url.toString(), 302);
   }
 
   // if size isn't recognised, redirect to default
   if (!supportedOgImageSizes.includes(size)) {
-    const url = new URL(request.url);
-    url.pathname = `/assets/${lang}/ogimage/v${OG_IMAGE_VERSION}/${defaultOgImageSize}/${slug}.${rev}.png`;
+    url.searchParams.set("size", `${defaultOgImageSize}`);
     return redirect(url.toString(), 302);
   }
 
   // if language isn't recognised, redirect to default
   if (!languages.includes(lang)) {
-    const url = new URL(request.url);
-    url.pathname = `/assets/${defaultLang}/ogimage/v${OG_IMAGE_VERSION}/${size}/${slug}.${rev}.png`;
+    url.searchParams.set("lang", `${defaultLang}`);
     return redirect(url.toString(), 302);
   }
 
@@ -72,11 +62,11 @@ export const loader: LoaderFunc<Ctx> = async ({ params, request }) => {
   // todo: maybe show `status` as well?
   const { data } = matter(source) as unknown as { data: Frontmatter };
   const { status, title, author } = data;
+  const titleHash = revHash(title);
 
-  // fixme: do we want to redirect to the current title instead? I guess so!
-  if (rev.toLowerCase() !== revHash(title).toLowerCase()) {
-    throw new Response("Not Found", { status: 404 });
-    // throw notFoundError(lang);
+  if (rev.toLowerCase() !== titleHash.toLowerCase()) {
+    url.searchParams.set("rev", `${titleHash}`);
+    return redirect(url.toString(), 302);
   }
 
   const buffer = await ogImageGenerator({ title, slug, lang, size, status, author });
@@ -87,7 +77,7 @@ export const loader: LoaderFunc<Ctx> = async ({ params, request }) => {
     "Content-Type": "image/png",
     "Access-Control-Expose-Headers": "Content-Disposition",
     // can be `inline` or `attachment`
-    "Content-Disposition": `${contentDisposition}; filename="canrau.com_${slug}_${lang}_ogimage-${size}-v${OG_IMAGE_VERSION}.png"`,
+    "Content-Disposition": `${contentDisposition}; filename="${domain}_${slug}_${lang}_ogimage-${size}-v${OG_IMAGE_VERSION}.png"`,
     "x-content-type-options": "nosniff",
     // "Cache-Control": "public,max-age=31536000,immutable",
   };
